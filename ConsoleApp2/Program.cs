d6b9a16e0402ed12022a28624ee4bc77b09b9a22
adjutantee <romashilov3233@gmail.com>
2022-04-05 10:17:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class MainClass
+{
+    static string GetFileHash(string fileName)
+    {
+        var content = File.ReadAllBytes(fileName);
+
+        return Convert.ToBase64String(content);
+    }
+
+    public static void Main()
+    {
+        string examplePath = @"C:\Users\Izagakhmaevra\Desktop\хеш файлы\769817.png";
+        string dirName = @"C:\Users\Izagakhmaevra\Desktop\хеш файлы";
+        string hash = GetFileHash(examplePath);
+
+        string[] files = Directory.GetFiles(dirName, "*.png", SearchOption.AllDirectories);
+
+        int hashs = 0;
+        int nonhash = 0;
+        foreach (string s in files)
+        {
+            if (GetFileHash(s) == hash)
+            {
+                Console.WriteLine(s);
+                hashs++;
+                File.Delete(s);
+            } 
+            else if (GetFileHash(s) != hash)
+            {
+                Console.WriteLine(s);
+                nonhash++;
+            }    
+        }
+        Console.WriteLine($"Всего обнаружено {hashs} hash файлов и {nonhash} nonhash файлов\nУдалено {hashs} hash файлов");
+    }
+}
